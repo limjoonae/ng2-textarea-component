0c2547b55b8adbfc7eebda57cc38f896fdbe3f21
Supakan Siwatmongkol <supakansiw@7eleven.cp.co.th>
2016-11-22 16:04:31 +0700

    edit for support class btn, btn-outline color

--- service/select-color-theme.service.ts
+++ service/select-color-theme.service.ts
@@ -3,11 +3,21 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class SelectColorThemeService {
 
-  private _colorTheme = "";
+  private _colorTheme = '';
 
-  getColorTheme(_colorClassPrefixInput:string, colorThemeInput: string): string {
-    if(colorThemeInput != null)
-      return _colorClassPrefixInput.concat(colorThemeInput);
+  getColorTheme(_colorClassPrefixInput:string, colorThemeInput: string, isOutline?: string): string {
+
+    let outlineClass = '';
+    let _colorThemeInput = '';
+    
+    if(isOutline == 'true'){
+      outlineClass = 'outline-'
+    }
+
+    if(colorThemeInput != null) {
+      _colorThemeInput = outlineClass.concat(colorThemeInput);
+      return _colorClassPrefixInput.concat(_colorThemeInput);
+    }
     return this._colorTheme;
   }
 
